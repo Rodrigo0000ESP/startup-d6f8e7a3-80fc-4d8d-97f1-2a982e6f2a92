be219f5d21d14107cb00582fb3492a64f81708c2
Rodrigo0000ESP <cordejavihd@gmail.com>
2025-04-20 19:25:49 +0100

    Delete backend directory

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-def process_hotel_receipt_image(image_path):
-    extracted_text = extract_text(image_path)
-    if not extracted_text:
-        return None
-    validated_data = validate_data(extracted_text)
-    if not validated_data:
-        return None
-    csv_data = generate_csv(validated_data)
-    return csv_data
--- backend/database/database.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE HotelReceipts (id INT, extracted_text TEXT);
-
--- TODO: Add SQL code here to map extracted data to customizable fields and generate CSV output
-
-SELECT * FROM HotelReceipts;
