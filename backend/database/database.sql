c03f49ca3e3e4a435dc6ea8ee4d602ab533b1c2b
Rodrigo0000ESP <cordejavihd@gmail.com>
2025-04-20 16:51:26 +0100

    Add backend/database/database.sql

---
+++ backend/database/database.sql
@@ -0,0 +1,2 @@
+1. Define SQL queries for creating tables to store hotel receipt information such as receipt date, amount, hotel name, etc.
+2. Execute the SQL queries to create the required database tables.
